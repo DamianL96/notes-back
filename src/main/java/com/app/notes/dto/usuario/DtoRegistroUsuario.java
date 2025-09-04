76924d656b1523508a7528eb9807a29e8ee82c8a
Damian Lambrecht <damianlambrecht@gmail.com>
2025-09-04 17:11:13 -0300

    usuario jpa, dto migracion

---
+++ src/main/java/com/app/notes/dto/usuario/DtoRegistroUsuario.java
@@ -0,0 +1,10 @@
+package com.app.notes.dto.usuario;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record DtoRegistroUsuario(
+        @NotBlank @Email String email,
+        @NotBlank String nombre,
+        @NotBlank String password
+){}
---
+++ src/main/java/com/app/notes/repository/UsuarioRepository.java
@@ -0,0 +1,4 @@
+package com.app.notes.repository;
+
+public interface UsuarioRepository {
+}
