1089781c712bcf489ad6d03e871999c6ccb9e583
Damian Lambrecht <damianlambrecht@gmail.com>
2025-09-28 00:01:30 -0300

    creacion de la entidad de nota
    con id
    sin constructores de dtos

---
+++ src/main/java/com/app/notes/Note/DtoCrearNota.java
@@ -0,0 +1,4 @@
+package com.app.notes.Note;
+
+public record DtoCrearNota() {
+}
--- src/main/java/com/app/notes/Note/Nota.java
+++ src/main/java/com/app/notes/Note/Nota.java
@@ -1,6 +1,31 @@
 package com.app.notes.Note;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Table(name="notas")
+@Entity(name="Nota")
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
+
 public class Nota {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String cuerpo;
+    private LocalDateTime fecha_creacion;
+    private LocalDateTime fecha_modificacion;
 
 
 }
