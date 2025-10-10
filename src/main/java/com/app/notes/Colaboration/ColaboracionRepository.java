12c84fe6b9fe7c453fa3f4a1e3569ec4d3ef336b
Damian Lambrecht <damianlambrecht@gmail.com>
2025-10-10 16:54:47 -0300

    Punto de control
    endpoints funcionales:
    
    auth: registrar, login
    nota: crear, mostrarDetalleNota, modificarNota, eliminarNota
    notaColaboracion: agregarColaborador, removerColaborador
    
    enpoints pendientes:
    usuario: mostrarDetalleUsuario, modificarUsuario
    colaboracion: mostrarColaboraciones(todas las notas de un usuario)
    notaColaboracion: mostrarColaboradoresDeUnaNota, modificarPermisos

--- src/main/java/com/app/notes/Colaboration/ColaboracionRepository.java
+++ src/main/java/com/app/notes/Colaboration/ColaboracionRepository.java
@@ -3,4 +3,5 @@ package com.app.notes.Colaboration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ColaboracionRepository extends JpaRepository<Colaboracion,Long> {
+
 }
