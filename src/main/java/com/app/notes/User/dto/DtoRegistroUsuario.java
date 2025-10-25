91149e6608b59b86914e7e1b38351f63975af0d8
Damian Lambrecht <damianlambrecht@gmail.com>
2025-10-25 16:15:02 -0300

    validaciones al registrar un usuario nuevo
    valida formato correcto de email
    valida que los campos requeridos no esten vacíos
    verifica que el email no se repita
    valida la longitud y complejidad de la contraseña
    genera automaticamente la fecha de registro del usuario

--- src/main/java/com/app/notes/User/dto/DtoRegistroUsuario.java
+++ src/main/java/com/app/notes/User/dto/DtoRegistroUsuario.java
@@ -2,9 +2,19 @@ package com.app.notes.User.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record DtoRegistroUsuario(
-        @NotBlank @Email String email,
-        @NotBlank String nombre,
-        @NotBlank String password
+        @NotBlank(message = "El email no puede estar vacio")
+        @Email(message = "Ingresa un Email válido")
+        String email,
+
+        @NotBlank(message = "El nombre no puede estar vacío")
+        String nombre,
+
+        @NotBlank(message ="El password no puede estar vacío")
+        @Size(min=8,max=24, message = "El password debe tener mas de 8 carácteres y menos de 24 carácteres")
+        @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d).+$", message = "El password debe contener al menos una letra mayúscula y un número")
+        String password
 ){}
--- src/main/java/com/app/notes/infrastructure/exceptions/AuthExceptionHandler.java
+++ src/main/java/com/app/notes/infrastructure/exceptions/AuthExceptionHandler.java
@@ -2,10 +2,12 @@ package com.app.notes.infrastructure.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 //Maneja las excepciones globalmente
 @ControllerAdvice
@@ -13,6 +15,7 @@ public class AuthExceptionHandler {
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
     public ResponseEntity<ErrorResponse> handleEmailExists(EmailAlreadyExistsException ex){
+
         ErrorResponse error = new ErrorResponse(
           HttpStatus.CONFLICT.value(),
           "EmailAlreadyExistException",
@@ -23,9 +26,28 @@ public class AuthExceptionHandler {
 
     }//devuelven el dto ErrorResponse
 
+
     @ExceptionHandler(InvalidCredentialsException.class)
     public ResponseEntity<String> handleInvalidCredentials(InvalidCredentialsException ex){
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationErrors(MethodArgumentNotValidException ex){
+        String errores = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": "+ error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "ValidationException",
+                errores,
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
 
 }
